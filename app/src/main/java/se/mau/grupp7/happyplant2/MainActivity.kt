35689a374c5a60c4a4e641a0625fe304bd1993a1
Kactuz <61052915+vasp03@users.noreply.github.com>
2026-01-30 10:33:18 +0100

    Played around and tested new things. Added a navbar and coresponding screens.

--- app/src/main/java/se/mau/grupp7/happyplant2/MainActivity.kt
+++ app/src/main/java/se/mau/grupp7/happyplant2/MainActivity.kt
@@ -3,43 +3,201 @@ package se.mau.grupp7.happyplant2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import android.widget.Toast
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import se.mau.grupp7.happyplant2.controller.BackendConnector
 import se.mau.grupp7.happyplant2.view.theme.HappyPlant2Theme
-import se.mau.grupp7.happyplant2.view.components.ButtonGrid
 
-private var backendConnector : BackendConnector? = null
+private var backendConnector: BackendConnector? = null
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setup()
 
-        enableEdgeToEdge()
         setContent {
             HappyPlant2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ButtonGrid(
-                        this@MainActivity,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
-fun setup(){
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    val plantList = remember { mutableStateListOf<String>() }
+
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("home") { BonsaiScreen() }
+            composable("plantList") {
+                GridScreen(
+                    plantNames = plantList,
+                    onAddPlant = { plantList.add("New Plant ${plantList.size + 1}") },
+                    gotoPlant = { navController.navigate("placeholder") }
+                )
+            }
+            composable("placeholder") { PlaceholderScreen() }
+            composable("addPlant") { AddPlantScreen() }
+        }
+    }
+}
+
+@Composable
+fun AddPlantScreen(){
+    Text("Hello")
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        NavigationItem("home", Icons.Default.Home, "Home"),
+        NavigationItem("plantList", Icons.AutoMirrored.Filled.List, "Grid"),
+        NavigationItem("placeholder", Icons.Default.Settings, "Placeholder")
+    )
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(text = item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+data class NavigationItem(val route: String, val icon: ImageVector, val title: String)
+
+@Composable
+fun BonsaiScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.bonsai_tree),
+            contentDescription = "Bonsai Tree"
+        )
+    }
+}
+
+@Composable
+fun GridScreen(plantNames: List<String>, onAddPlant: () -> Unit, gotoPlant: () -> Unit) {
+    Column(Modifier.fillMaxSize()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(8.dp),
+            modifier = Modifier.weight(1f) // Make the grid take up all available space
+        ) {
+            items(plantNames) { plantName ->
+                PlantCard(plantName)
+            }
+        }
+        Button(
+            onClick = onAddPlant,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp) // Stick to the bottom
+        ) {
+            Text(text = "Quick Add New Plant")
+        }
+        Button(
+            onClick = gotoPlant,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp) // Stick to the bottom
+        ) {
+            Text(text = "Goto Add Plant")
+        }
+    }
+}
+
+@Composable
+fun PlantCard(plantName: String) {
+    Card(modifier = Modifier.padding(8.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = plantName)
+        }
+    }
+}
+
+@Composable
+fun PlaceholderScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "No plant has been selected")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HappyPlant2Theme {
+        MainScreen()
+    }
+}
+
+
+fun setup() {
     // Setup is run here.
     // Java classes that connects to backend server is initialize here.
     backendConnector = BackendConnector()
