c6db54cf6f561ac5ca22df57847a8c0d54ecdfe7
kevintav <kevin@amanzi.com>
2026-02-09 19:48:11 +0100

    Removed unused imports

--- apk/app/src/main/java/se/mau/grupp7/happyplant2/MainActivity.kt
+++ apk/app/src/main/java/se/mau/grupp7/happyplant2/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
