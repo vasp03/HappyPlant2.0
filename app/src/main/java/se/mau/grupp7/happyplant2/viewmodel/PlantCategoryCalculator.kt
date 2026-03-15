5212d5de41bf78acf641dde37d8b5e55a64f4f79
love skön <loveskon335@gmail.com>
2026-03-15 13:54:09 +0100

    Small change in category function to fix case-sensitive failing test.

--- app/src/main/java/se/mau/grupp7/happyplant2/viewmodel/PlantCategoryCalculator.kt
+++ app/src/main/java/se/mau/grupp7/happyplant2/viewmodel/PlantCategoryCalculator.kt
@@ -4,9 +4,8 @@ import se.mau.grupp7.happyplant2.model.UserPlant
 
 internal fun calculateCategories(plants: List<UserPlant>): List<String> {
     return plants
-        .map {it.category.trim()}
-        .filter {it.isNotBlank()}
-        .distinct()
-        .sorted()
-
+        .map { it.category.trim() }
+        .filter { it.isNotBlank() }
+        .distinctBy { it.lowercase() }
+        .sortedBy { it.lowercase() }
 }
