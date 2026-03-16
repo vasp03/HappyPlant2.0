32647e0bca800cbc396b019176ce6ef88d20c698
AlperEken <alpereken04@gmail.com>
2026-03-16 14:42:33 +0100

    Changes in imports to prevent errors in MainActivity

--- app/src/main/java/se/mau/grupp7/happyplant2/MainActivity.kt
+++ app/src/main/java/se/mau/grupp7/happyplant2/MainActivity.kt
@@ -18,7 +18,7 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getBy
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
